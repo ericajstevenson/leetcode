5eeae70bc47e0b29eb4c6817b76a4adc7cc001a4
Erica Stevenson <ericajstevenson@gmail.com>
2023-02-15 18:20:06 -0800

    update current leetcode problem

--- 160.py
+++ 160.py
@@ -4,12 +4,25 @@
 #         self.val = x
 #         self.next = None
 
+# solution 1: use a hash map (set)
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        L1 = headA
-        L2 = headB
-        while L1 != L2:
-            L1 = L1.next if L1 else return null # if L1 is not null, move to the next node
-            L2 = L2.next if L2 else return null
-        if L1 == L2:
-            return L1
+        seen = set() # set or hash map
+        l1,l2 = headA, headB
+        while l1:
+            seen.add(l1)
+            l1 = l1.next
+        while l2:
+            if l2 in seen:
+                return l2
+            l2 = l2.next
+        return l1 # will return null if there is no intersection
+
+# solution 2: use two pointers to traverse l1 into l2 and vice versa (they will meet at the intersection)
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        l1,l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1 # if they are equal, return either one
