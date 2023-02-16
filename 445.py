f5abc6b4c9de432c397fffc7b5f0f093f1930351
Erica Stevenson <ericajstevenson@gmail.com>
2023-02-15 18:19:00 -0800

    incomplete!

---
+++ 445.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1,num2 = 0,0
+
+        while l1:
+            num1 = num1*10 + l1.val
+            l1 = l1.next
+
+        while l1:
+            num1 = num1*10 + l1.val
+            l1 = l1.next
