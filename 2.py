12625d591d868b9e9cb7da811c2972971811096e
Erica Stevenson <ericajstevenson@gmail.com>
2023-02-19 23:50:43 -0800

    update current leetcode problem

--- 2.py
+++ 2.py
@@ -5,5 +5,16 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        n1, n2 = "", ""
-        
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
+        while l1 or l2: # whiel there is still a number in one of the nodes
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            total = v1 + v2 + carry
+            carry = total // 10 # get the tens place for the next carry
+            total = total % 10 # get the ones place for the current node
+            cur.next = ListNode(total)
+
+
+
