28c0423fae550af205155b56bff8f08fca210faa
Erica Stevenson <ericajstevenson@gmail.com>
2023-02-23 19:53:09 -0800

    update new problem

--- 67.py
+++ 67.py
@@ -5,18 +5,16 @@ class Solution:
         carry = 0
         a,b = a[::-1],b[::-1] # reverse the strings
         for i in range(max(len(a),len(b))):
-            print(i)
-            if int(a[i])+int(b[i])+carry==0:
-                res += '0'
-                carry = 0
-                print(res)
-            elif int(a[i])+int(b[i])+carry==1:
-                res += '1'
-                carry = 0
-                print(res)
-            elif int(a[i])+int(b[i])+carry==3:
-                res += '0'
-                carry = 1
-                print(res)
-        print(res[::-1])
-print(Solution().addBinary('10','1101011'))
+            # here i subtract ord('0') to get the int from ascii val
+            digitA = ord(a[i]) - ord('0') if i < len(a) else 0
+            print(digitA)
+            digitB = ord(b[i]) - ord('0') if i < len(b) else 0
+            total = digitB + digitA + carry
+            char = str(total % 2)
+            res += char + res
+            carry = total // 2
+
+            if carry: # edge case: if carry is not 0
+                res += '1' + res
+        return res
+print(Solution().addBinary('10','11'))
