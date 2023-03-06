cf975ca7795c9c9f348a30959002b2f0d7382a20
Erica Stevenson <ericajstevenson@gmail.com>
2023-03-05 21:43:38 -0800

    update new problem

---
+++ 58.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        i = len(s) - 1
+        while s[i] == ' ': # edge case where s ends with spaces
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+        return length
+
+
+print(Solution().lengthOfLastWord("Hello World"))
