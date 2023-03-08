c4884536452f601acc99a48a15745fbc2eec9dd9
Erica Stevenson <ericajstevenson@gmail.com>
2023-03-07 19:00:30 -0800

    update new problem

--- 108.py
+++ 108.py
@@ -8,15 +8,13 @@ class TreeNode:
         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        def helper(l,r):
-            if l > r: # base case
-                return None
-            mid = (l + r) // 2
-            root = TreeNode(nums[mid]) # create a new node at the midpoint
-            root.left = helper(l, mid-1)
-            root.right = helper(mid+1, r)
-            return root
-        return helper(0, len(nums)-1)
+        if not nums: # edge case nums is empty
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
 
 
 print(Solution().sortedArrayToBST(nums = [-10,-3,0,5,9]))
