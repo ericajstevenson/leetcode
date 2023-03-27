7804522077ce0afa8580d07359fc4dfccd5a4c4d
Erica Stevenson <ericajstevenson@gmail.com>
2023-03-27 15:57:22 -0700

    update new problem

---
+++ 3.py
@@ -0,0 +1,20 @@
+# solve using sliding window techique with complexity O(n)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest_substring = 0
+        charSet = set()
+        l = 0
+
+        for r in range(len(s)): # for right pointer in len s
+            while s[r] in charSet: # if there is a duplicate already in charset
+                charSet.remove(s[l]) # remove leftmost character
+                l += 1
+            charSet.add(s[r]) # add newest char on to right
+            longest_substring = max(longest_substring,len(charSet))
+        return longest_substring
+
+
+
+
+
+
