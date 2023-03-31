2e5f5aec5774344e5c23ea2fb5255059e44abd8c
Erica Stevenson <ericajstevenson@gmail.com>
2023-03-31 11:23:36 -0700

    update new problem

--- 16.py
+++ 16.py
@@ -1,32 +1,28 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) <3:
-            return []
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+
+        closest = 100000
         nums.sort()
-        res = []
 
         for i, val in enumerate(nums):
-            if nums[i]>0:
-                break
             if i>0 and val == nums[i-1]: # if i is not the first number and it's not a repeated value
                 continue
 
             l,r = i+1,len(nums)-1
             while l < r:
                 threeSum = val + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([val,nums[l],nums[r]])
+                if threeSum == target:
+                    return threeSum
+                elif abs(threeSum - target) < abs(closest - target):
+                    closest = threeSum
+                elif threeSum <= target:
                     l += 1
                     while nums[l] == nums[l-1] and l<r:
                         l += 1
-        return res
+        return closest
 
 
-                #
+
 
 
 
